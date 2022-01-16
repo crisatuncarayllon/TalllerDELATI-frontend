1b81fc97976508f8b3ae8f0d9b137308dc4bf415
crisatuncarayllon <58598125+crisatuncarayllon@users.noreply.github.com>
2022-01-16 02:46:59 -0500

    Backend integrado
    El backend ya se encuentra en la nube y ya esta integrado el proyecto con su codigo

--- src/services/machine-learning-weka.service.ts
+++ src/services/machine-learning-weka.service.ts
@@ -7,8 +7,9 @@ import { Injectable } from '@angular/core';
 export class MachineLearningWekaService {
   //readonly URL = 'https://delati-pmljs17v1-backend.herokuapp.com';
   //readonly URL = 'http://127.0.0.1:5000'; 
-  //readonly URL = 'https://delatiback.herokuapp.com/';
-  readonly URL = 'http://localhost:8080';
+  readonly URL = 'https://delatibackweka.herokuapp.com/';
+  //readonly URL = 'http://localhost:8080';
+  
   constructor(private http: HttpClient) { }
 
   getAlgorithms(){ return this.http.get(this.URL + '/algorithms'); }
